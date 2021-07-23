854428b7022fb8fee7f01a8bf7eee25788d1c2cc
clowden28 <Jcpennies1!>
2021-07-23 14:55:02 -0400

    created a backing property for currentScrambledWord in GameFragment.kt and GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,4 +4,11 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
+    private var scpre = 0
+    private var currentWordCount = 0
+
+    //Creates a backing property for currentScrabmledWord
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
