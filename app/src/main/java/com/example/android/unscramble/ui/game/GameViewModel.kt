2be9ea1bcc4aaa05ee7449b47979b745c6df5506
clowden28 <Jcpennies1!>
2021-07-26 12:01:22 -0400

    added fun getNextWord/ while and if else statement to GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -2,19 +2,18 @@ package com.example.android.unscramble.ui.game
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+//Adds a lateinit modifier to _currentScrambledWord
+private lateinit var _currentScrambledWord: String
 
 class GameViewModel : ViewModel() {
     init {
         Log.d("GameFragment", "GameViewModel created!")
     }
 
-    /**
-     *
-     */
-
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+
     }
     private var scpre = 0
     private var currentWordCount = 0
@@ -26,7 +25,8 @@ class GameViewModel : ViewModel() {
     //Creates a new class variable to hold a list of words and avoid repetitions
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
-    /*
+
+    /**
      * Updates currentWord and currentScrambledWord with the next word.
      */
     private fun getNextWord() {
@@ -46,5 +46,22 @@ class GameViewModel : ViewModel() {
                 ++currentWordCount
                 wordsList.add(currentWord)
             }
+
+    }
+
+    init  {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+
+    }
+    /**
+    * Returns true if the current word count is less than MAX_NO_OF_WORDS.
+    * Updates the next word.
+    */
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        }else false
     }
 }
