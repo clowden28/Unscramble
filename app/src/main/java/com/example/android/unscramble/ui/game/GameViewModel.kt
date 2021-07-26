07902292638cb7ae799cee47a640a87fcd76272a
clowden28 <Jcpennies1!>
2021-07-26 08:06:33 -0400

    Aded currentWord and getNextWord to GameViewModel.kt

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -8,6 +8,10 @@ class GameViewModel : ViewModel() {
         Log.d("GameFragment", "GameViewModel created!")
     }
 
+    /**
+     *
+     */
+
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
@@ -19,4 +23,14 @@ class GameViewModel : ViewModel() {
     private var _currentScrambledWord = "test"
     val currentScrambledWord: String
         get() = _currentScrambledWord
+    //Creates a new class variable to hold a list of words and avoid repetitions
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+    //
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+    }
+
 }
